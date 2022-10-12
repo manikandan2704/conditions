8e8432a36828672f740fb62f2928ad37e22e61e4
manikandan <manimani19508@gmail.com>
2022-10-12 15:55:14 +0530

    add new file

--- js/script.js
+++ js/script.js
@@ -24,40 +24,44 @@ if (c<12) {
 
   //else if condition//
 
-  /*var hour=parseInt(prompt("enter the value"))
-  var c=hour
-  if (c < 12) {
-    console.log ("Good morning");
-  } else if (c<18) {
-    console.log("Good day")
-  } else {
-    console.log("Good evening");
-  }*/
-
+//   var age=(prompt("enter the age"))
+//  if(age<18){
+//   var permission=prompt("yes or no");
+//   if(permission==="yes"){
+//     console.log("allow with parent permission")
+//   }
+//  }
+//  else if(age>=18 && age<50){
+//   console.log("you are allowed")
+//  }
+//  else{
+//   console.log("you are not allowed")
+//  }
 
 
   //switch condition//
 
-  /*let day;
-switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-    day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case  6:
-    day = "Saturday";
-}*/
+//   let day=parseInt(prompt("enter your day"))
+// switch (day) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case  6:
+//     day = "Saturday";
+// }
+// console.log(day);
